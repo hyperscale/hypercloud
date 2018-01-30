b3cd5426664e32d276ec0177dd1fd487a3a4ca0f
Axel Etcheverry <axel@etcheverry.biz>
2018-01-30 23:20:12 +0100

    feat(hyperpaas-installer): Added dirty stack deploy

--- cmd/hyperpaas-installer/controller/installer_controller.go
+++ cmd/hyperpaas-installer/controller/installer_controller.go
@@ -5,16 +5,34 @@
 package controller
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
+	"sort"
 	"time"
 
+	"github.com/docker/cli/cli/compose/convert"
+	composetypes "github.com/docker/cli/cli/compose/types"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/swarm"
+	dockerclient "github.com/docker/docker/client"
 	"github.com/euskadi31/go-server"
 	"github.com/euskadi31/go-sse"
 	"github.com/hyperscale/hyperpaas/cmd/hyperpaas-installer/assets"
 	"github.com/hyperscale/hyperpaas/docker"
 	"github.com/hyperscale/hyperpaas/docker/compose"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
+)
+
+// Resolve image constants
+const (
+	defaultNetworkDriver = "overlay"
+	ResolveImageAlways   = "always"
+	ResolveImageChanged  = "changed"
+	ResolveImageNever    = "never"
 )
 
 // Event struct
@@ -46,6 +64,9 @@ func (c InstallerController) Mount(r *server.Router) {
 }
 
 func (c *InstallerController) postInstallerHandler(w http.ResponseWriter, r *http.Request) {
+	// ctx := r.Context()
+	ctx := context.Background()
+
 	if err := r.ParseMultipartForm(2048); err != nil {
 		server.FailureFromError(w, http.StatusRequestEntityTooLarge, err)
 
@@ -79,11 +100,76 @@ func (c *InstallerController) postInstallerHandler(w http.ResponseWriter, r *htt
 		Type: "configured",
 	}
 
-	//@TODO exec deploy stack
+	go func() {
+		namespace := convert.NewNamespace("hyperpaas")
 
-	c.events <- Event{
-		Type: "finished",
-	}
+		{
+			// prune services
+			services := map[string]struct{}{}
+			for _, service := range composeConfig.Services {
+				services[service.Name] = struct{}{}
+			}
+			pruneServices(ctx, c.dockerClient, namespace, services)
+		}
+
+		serviceNetworks := getServicesDeclaredNetworks(composeConfig.Services)
+		networks, externalNetworks := convert.Networks(namespace, composeConfig.Networks, serviceNetworks)
+		if err := validateExternalNetworks(ctx, c.dockerClient, externalNetworks); err != nil {
+			log.Error().Err(err).Msg("validateExternalNetworks")
+
+			return
+		}
+
+		if err := createNetworks(ctx, c.dockerClient, namespace, networks); err != nil {
+			log.Error().Err(err).Msg("createNetworks")
+
+			return
+		}
+
+		secrets, err := convert.Secrets(namespace, composeConfig.Secrets)
+		if err != nil {
+			log.Error().Err(err).Msg("Secrets")
+
+			return
+		}
+
+		if err := createSecrets(ctx, c.dockerClient, secrets); err != nil {
+			log.Error().Err(err).Msg("createSecrets")
+
+			return
+		}
+
+		configs, err := convert.Configs(namespace, composeConfig.Configs)
+		if err != nil {
+			log.Error().Err(err).Msg("Configs")
+
+			return
+		}
+
+		if err := createConfigs(ctx, c.dockerClient, configs); err != nil {
+			log.Error().Err(err).Msg("createConfigs")
+
+			return
+		}
+
+		services, err := convert.Services(namespace, composeConfig, c.dockerClient.Client)
+		if err != nil {
+			log.Error().Err(err).Msg("Services")
+
+			return
+		}
+
+		if err := deployServices(ctx, c.dockerClient, services, namespace, true, ResolveImageAlways); err != nil {
+			log.Error().Err(err).Msg("deployServices")
+
+			//@TODO send Error Event
+			return
+		}
+
+		c.events <- Event{
+			Type: "finished",
+		}
+	}()
 
 	server.JSON(w, http.StatusOK, composeConfig)
 }
@@ -108,4 +194,317 @@ func (c *InstallerController) getEventsHandler(rw sse.ResponseWriter, r *http.Re
 			return
 		}
 	}
+}
+
+// pruneServices removes services that are no longer referenced in the source
+func pruneServices(ctx context.Context, client *docker.Client, namespace convert.Namespace, services map[string]struct{}) {
+	oldServices, err := getServices(ctx, client, namespace.Name())
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to list services")
+	}
+
+	pruneServices := []swarm.Service{}
+
+	for _, service := range oldServices {
+		if _, exists := services[namespace.Descope(service.Spec.Name)]; !exists {
+			pruneServices = append(pruneServices, service)
+		}
+	}
+
+	removeServices(ctx, client, pruneServices)
+}
+
+func removeServices(ctx context.Context, client *docker.Client, services []swarm.Service) bool {
+	var hasError bool
+
+	sort.Slice(services, sortServiceByName(services))
+
+	for _, service := range services {
+		log.Info().Msgf("Removing service %s", service.Spec.Name)
+
+		if err := client.ServiceRemove(ctx, service.ID); err != nil {
+			hasError = true
+
+			log.Error().Err(err).Msgf("Failed to remove service %s", service.ID)
+		}
+	}
+
+	return hasError
+}
+
+func sortServiceByName(services []swarm.Service) func(i, j int) bool {
+	return func(i, j int) bool {
+		return services[i].Spec.Name < services[j].Spec.Name
+	}
+}
+
+func removeNetworks(ctx context.Context, client *docker.Client, networks []types.NetworkResource) bool {
+	var hasError bool
+
+	for _, network := range networks {
+		log.Info().Msgf("Removing network %s", network.Name)
+
+		if err := client.NetworkRemove(ctx, network.ID); err != nil {
+			hasError = true
+
+			log.Error().Err(err).Msgf("Failed to remove network %s", network.ID)
+		}
+	}
+
+	return hasError
+}
+
+func removeSecrets(ctx context.Context, client *docker.Client, secrets []swarm.Secret) bool {
+	var hasError bool
+
+	for _, secret := range secrets {
+		log.Info().Msgf("Removing secret %s", secret.Spec.Name)
+
+		if err := client.SecretRemove(ctx, secret.ID); err != nil {
+			hasError = true
+
+			log.Error().Err(err).Msgf("Failed to remove secret %s", secret.ID)
+		}
+	}
+
+	return hasError
+}
+
+func removeConfigs(ctx context.Context, client *docker.Client, configs []swarm.Config) bool {
+	var hasError bool
+
+	for _, config := range configs {
+		log.Info().Msgf("Removing config %s", config.Spec.Name)
+
+		if err := client.ConfigRemove(ctx, config.ID); err != nil {
+			hasError = true
+
+			log.Error().Err(err).Msgf("Failed to remove config %s", config.ID)
+		}
+	}
+
+	return hasError
+}
+
+func getServicesDeclaredNetworks(serviceConfigs []composetypes.ServiceConfig) map[string]struct{} {
+	serviceNetworks := map[string]struct{}{}
+
+	for _, serviceConfig := range serviceConfigs {
+		if len(serviceConfig.Networks) == 0 {
+			serviceNetworks["default"] = struct{}{}
+			continue
+		}
+
+		for network := range serviceConfig.Networks {
+			serviceNetworks[network] = struct{}{}
+		}
+	}
+
+	return serviceNetworks
+}
+
+func validateExternalNetworks(ctx context.Context, client dockerclient.NetworkAPIClient, externalNetworks []string) error {
+	for _, networkName := range externalNetworks {
+		if !container.NetworkMode(networkName).IsUserDefined() {
+			// Networks that are not user defined always exist on all nodes as
+			// local-scoped networks, so there's no need to inspect them.
+			continue
+		}
+
+		network, err := client.NetworkInspect(ctx, networkName, types.NetworkInspectOptions{})
+
+		switch {
+		case dockerclient.IsErrNotFound(err):
+			return errors.Errorf("network %q is declared as external, but could not be found. You need to create a swarm-scoped network before the stack is deployed", networkName)
+		case err != nil:
+			return err
+		case network.Scope != "swarm":
+			return errors.Errorf("network %q is declared as external, but it is not in the right scope: %q instead of \"swarm\"", networkName, network.Scope)
+		}
+	}
+
+	return nil
+}
+
+func createSecrets(ctx context.Context, client *docker.Client, secrets []swarm.SecretSpec) error {
+	for _, secretSpec := range secrets {
+		secret, _, err := client.SecretInspectWithRaw(ctx, secretSpec.Name)
+
+		switch {
+		case err == nil:
+			// secret already exists, then we update that
+			if err := client.SecretUpdate(ctx, secret.ID, secret.Meta.Version, secretSpec); err != nil {
+				return errors.Wrapf(err, "failed to update secret %s", secretSpec.Name)
+			}
+		case dockerclient.IsErrNotFound(err):
+			// secret does not exist, then we create a new one.
+
+			log.Info().Msgf("Creating secret %s", secretSpec.Name)
+
+			if _, err := client.SecretCreate(ctx, secretSpec); err != nil {
+				return errors.Wrapf(err, "failed to create secret %s", secretSpec.Name)
+			}
+		default:
+			return err
+		}
+	}
+
+	return nil
+}
+
+func createConfigs(ctx context.Context, client *docker.Client, configs []swarm.ConfigSpec) error {
+	for _, configSpec := range configs {
+		config, _, err := client.ConfigInspectWithRaw(ctx, configSpec.Name)
+
+		switch {
+		case err == nil:
+			// config already exists, then we update that
+			if err := client.ConfigUpdate(ctx, config.ID, config.Meta.Version, configSpec); err != nil {
+				return errors.Wrapf(err, "failed to update config %s", configSpec.Name)
+			}
+		case dockerclient.IsErrNotFound(err):
+			// config does not exist, then we create a new one.
+
+			log.Info().Msgf("Creating config %s", configSpec.Name)
+			if _, err := client.ConfigCreate(ctx, configSpec); err != nil {
+				return errors.Wrapf(err, "failed to create config %s", configSpec.Name)
+			}
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func getStackNetworks(
+	ctx context.Context,
+	client dockerclient.APIClient,
+	namespace string,
+) ([]types.NetworkResource, error) {
+	return client.NetworkList(ctx, types.NetworkListOptions{Filters: getStackFilter(namespace)})
+}
+
+func createNetworks(ctx context.Context, client *docker.Client, namespace convert.Namespace, networks map[string]types.NetworkCreate) error {
+	existingNetworks, err := getStackNetworks(ctx, client, namespace.Name())
+	if err != nil {
+		return err
+	}
+
+	existingNetworkMap := make(map[string]types.NetworkResource)
+	for _, network := range existingNetworks {
+		existingNetworkMap[network.Name] = network
+	}
+
+	for internalName, createOpts := range networks {
+		name := namespace.Scope(internalName)
+		if _, exists := existingNetworkMap[name]; exists {
+			continue
+		}
+
+		if createOpts.Driver == "" {
+			createOpts.Driver = defaultNetworkDriver
+		}
+
+		log.Info().Msgf("Creating network %s", name)
+		if _, err := client.NetworkCreate(ctx, name, createOpts); err != nil {
+			return errors.Wrapf(err, "failed to create network %s", internalName)
+		}
+	}
+
+	return nil
+}
+
+func deployServices(
+	ctx context.Context,
+	client *docker.Client,
+	services map[string]swarm.ServiceSpec,
+	namespace convert.Namespace,
+	sendAuth bool,
+	resolveImage string,
+) error {
+	existingServices, err := getServices(ctx, client, namespace.Name())
+	if err != nil {
+		return err
+	}
+
+	existingServiceMap := make(map[string]swarm.Service)
+	for _, service := range existingServices {
+		existingServiceMap[service.Spec.Name] = service
+	}
+
+	for internalName, serviceSpec := range services {
+		name := namespace.Scope(internalName)
+
+		encodedAuth := ""
+		image := serviceSpec.TaskTemplate.ContainerSpec.Image
+		if sendAuth {
+			// Retrieve encoded auth token from the image reference
+			/*encodedAuth, err = command.RetrieveAuthTokenFromImage(ctx, dockerCli, image)
+			if err != nil {
+				return err
+			}*/
+		}
+
+		if service, exists := existingServiceMap[name]; exists {
+			log.Info().Msgf("Updating service %s (id: %s)", name, service.ID)
+
+			updateOpts := types.ServiceUpdateOptions{EncodedRegistryAuth: encodedAuth}
+
+			switch {
+			case resolveImage == ResolveImageAlways || (resolveImage == ResolveImageChanged && image != service.Spec.Labels[convert.LabelImage]):
+				// image should be updated by the server using QueryRegistry
+				updateOpts.QueryRegistry = true
+			case image == service.Spec.Labels[convert.LabelImage]:
+				// image has not changed; update the serviceSpec with the
+				// existing information that was set by QueryRegistry on the
+				// previous deploy. Otherwise this will trigger an incorrect
+				// service update.
+				serviceSpec.TaskTemplate.ContainerSpec.Image = service.Spec.TaskTemplate.ContainerSpec.Image
+			}
+			response, err := client.ServiceUpdate(
+				ctx,
+				service.ID,
+				service.Version,
+				serviceSpec,
+				updateOpts,
+			)
+			if err != nil {
+				return errors.Wrapf(err, "failed to update service %s", name)
+			}
+
+			for _, warning := range response.Warnings {
+				log.Error().Msg(warning)
+			}
+		} else {
+			log.Info().Msgf("Creating service %s", name)
+
+			createOpts := types.ServiceCreateOptions{EncodedRegistryAuth: encodedAuth}
+
+			// query registry if flag disabling it was not set
+			if resolveImage == ResolveImageAlways || resolveImage == ResolveImageChanged {
+				createOpts.QueryRegistry = true
+			}
+
+			if _, err := client.ServiceCreate(ctx, serviceSpec, createOpts); err != nil {
+				return errors.Wrapf(err, "failed to create service %s", name)
+			}
+		}
+	}
+
+	return nil
+}
+
+func getStackFilter(namespace string) filters.Args {
+	filter := filters.NewArgs()
+	filter.Add("label", convert.LabelNamespace+"="+namespace)
+
+	return filter
+}
+
+func getServiceFilter(namespace string) filters.Args {
+	return getStackFilter(namespace)
+}
+
+func getServices(ctx context.Context, client *docker.Client, namespace string) ([]swarm.Service, error) {
+	return client.ServiceList(ctx, types.ServiceListOptions{Filters: getServiceFilter(namespace)})
 }
